c60fe7c8884ea3ec7a80ddc24722322028f48756
twenz235 <t.wen.z.235@gmail.com>
2022-02-14 01:39:23 +0700

    .

--- src/components/CPM.js
+++ src/components/CPM.js
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row, Skeleton } from 'antd';
+import { Button, Card, Col, PageHeader, Row, Skeleton } from 'antd';
 import { isEmpty } from 'lodash';
 import React from 'react';
 import { useSelector, useStore } from 'react-redux';
@@ -32,40 +32,45 @@ const CPM = () => {
   }
 
   return (
-    <Row justify='space-around' style={{ padding: '30px' }}>
-      <Col xs={24} md={8}>
-        <Row >
-          <Col span={24}>
-            <Row justify='center'>
-              <Button className='exchBtn' onClick={() => initBtn(BTC_THB)}>
-                BTC_THB
-              </Button>
-              <Button className='exchBtn' onClick={() => initBtn(USDT_THB)}>
-                USDT_THB
-              </Button>
-              <Button className='exchBtn' onClick={() => initBtn(BUSD_THB)}>
-                BUSD_THB
-              </Button>
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-      <Col xs={24} md={16}>
-        <Card title={param.exc || 'Select One'} style={{ width: '100%' }} extra={<a href="#">More</a>} >
-          {
-            loading ? <Skeleton />
-              : (
-                <>
-                  <p>{data?.lastPrice}</p>
-                  <p>{data?.volume}</p>
-                  <p>{data?.symbol}</p>
-                  {!data?.symbol ? <p>Coming Soon</p> : ''}
-                </>
-              )
-          }
-        </Card>
-      </Col>
-    </Row>
+    <>
+      <Row>
+    <PageHeader title='Back' onBack={() => nav(-1) } ></PageHeader>
+      </Row>
+      <Row justify='space-around' style={{ padding: '30px' }}>
+        <Col xs={24} md={8}>
+          <Row >
+            <Col span={24}>
+              <Row justify='center'>
+                <Button className='exchBtn' onClick={() => initBtn(BTC_THB)}>
+                  BTC_THB
+                </Button>
+                <Button className='exchBtn' onClick={() => initBtn(USDT_THB)}>
+                  USDT_THB
+                </Button>
+                <Button className='exchBtn' onClick={() => initBtn(BUSD_THB)}>
+                  BUSD_THB
+                </Button>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} md={16}>
+          <Card title={param.exc || 'Select One'} style={{ width: '100%' }} extra={<a href="#">More</a>} >
+            {
+              loading ? <Skeleton />
+                : (
+                  <>
+                    <p>{data?.lastPrice}</p>
+                    <p>{data?.volume}</p>
+                    <p>{data?.symbol}</p>
+                    {!data?.symbol ? <p>Coming Soon</p> : ''}
+                  </>
+                )
+            }
+          </Card>
+        </Col>
+      </Row>
+    </>
   )
 }
 export default CPM
